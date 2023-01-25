632147eb65abd3f64dae50356b7ed22c37f39810
luiz-08 <luiz.gustavo2928@gmail.com>
2023-01-24 23:09:07 -0300

    adição do widget calendarView e configurado/testado o clique de evento definido com view binding

--- app/src/main/java/com/example/churchcalendar/ui/calendario/calendarioFragment.kt
+++ app/src/main/java/com/example/churchcalendar/ui/calendario/calendarioFragment.kt
@@ -1,31 +1,54 @@
 package com.example.churchcalendar.ui.calendario
 
+import android.app.Fragment
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CalendarView
 import android.widget.TextView
-import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.churchcalendar.R
 import com.example.churchcalendar.databinding.FragmentCalendarioBinding
+import java.text.NumberFormat.getInstance
+import java.util.*
+import java.util.Calendar.getInstance
 
-
-class calendarioFragment : Fragment() {
+class calendarioFragment : androidx.fragment.app.Fragment() {
 
     private lateinit var binding: FragmentCalendarioBinding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
+
+
     ): View? {
-        binding = FragmentCalendarioBinding.inflate(inflater, container, false)
+
+        binding = FragmentCalendarioBinding.inflate(layoutInflater)
+        val currentDate = Calendar.getInstance()
+        binding.calendarioIgreja.date = currentDate.timeInMillis
+
+
+        val events = mutableMapOf<Calendar, String>()
+        binding.calendarioIgreja.setOnDateChangeListener {view, year, month, dayofMonth ->
+            val selectedDate = Calendar.getInstance().apply {
+                set(Calendar.YEAR, year)
+                set(Calendar.MONTH, month)
+                set(Calendar.DAY_OF_MONTH, dayofMonth)
+            }
+
+            events[Calendar.getInstance().apply {
+                set(Calendar.YEAR, 2023)
+                set(Calendar.MONTH, Calendar.JANUARY)
+                set(Calendar.DAY_OF_MONTH, 10)
+            }] = "ANIVERSÁRIO DO BRABO AQUI MEU LINDO!!"
+
+            val event = events[selectedDate]
+            binding.Eventodata.text = event
+        }
         return binding.root
     }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        binding.textCalendario.text = "CALENDÁRIO AQUI! EM PRODUÇÃO"
-    }
 }
+
 
--- app/src/main/java/com/example/churchcalendar/ui/departamentos/departamentosFragment.kt
+++ app/src/main/java/com/example/churchcalendar/ui/departamentos/departamentosFragment.kt
@@ -1,11 +1,11 @@
 package com.example.churchcalendar.ui.departamentos
 
+import android.app.Fragment
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.churchcalendar.databinding.FragmentDepartamentosBinding
 
@@ -23,10 +23,5 @@ class departamentosFragment : Fragment() {
 
         binding = FragmentDepartamentosBinding.inflate(inflater,container,false)
         return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        binding.textDepartamentos.text = "Aqui será os departamentos! Em produção"
     }
 }
